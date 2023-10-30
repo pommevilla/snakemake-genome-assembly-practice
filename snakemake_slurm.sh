0c1aafb2d1fb92cd3284d58ed372e2813cba968d
Paul Villanueva <pvillanueva13@gmail.com>
2023-10-30 17:04:17 -0500

    Update initial parameters

--- snakemake_slurm.sh
+++ snakemake_slurm.sh
@@ -3,15 +3,18 @@
 #SBATCH -p medium
 #SBATCH --mem=100gb
 #SBATCH -N 1
-#SBATCH -n 32
-#SBATCH -t 72:00:00
+#SBATCH -n 48
+#SBATCH -t 48:00:00
 #SBATCH -o "logs/slurm/out/stdout.%j.%N"
 #SBATCH -e "logs/slurm/err/stderr.%j.%N"
 #SBATCH --mail-user=paul.villanueva@usda.gov
 #SBATCH --mail-type=BEGIN,END,FAIL
 
 cd /project/fsepru/paul.villanueva/repos/snakemake-genome-assembly-practice
+source /home/${USER}/.bashrc
+source activate snakemake
+
 
 date
-time snakemake --use-conda --profile slurm
+time snakemake --use-conda --profile slurm -p
 date
